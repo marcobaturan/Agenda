5f90b34a912bb679128e2e214c8f1293a7824e40
Marco Baturan <marco.baturan@gmail.com>
2019-10-01 15:43:48 +0200

    Opload tests and module.
    In advance, first-ever develope the visible part.

--- agenda.py
+++ agenda.py
@@ -4,3 +4,74 @@ from peewee import *
 import sys
 
 
+# GUI
+# crear menu
+def menu():
+    msg = 'Select an option.'
+    title = 'Menu'
+    choices = ["Search","Modify"]
+    choice = choicebox(msg,title,choices)
+# ventana busqueda
+def search():
+    msg = "Enter the information"
+    title = "Search"
+    fieldNames = ["Name","Telephone","E-mail"]
+    fieldValues = []  # we start with blanks for the values
+    fieldValues = multenterbox(msg,title, fieldNames)
+# ventana registro
+def registry():
+    msg = "Enter the information"
+    title = "Registry"
+    fieldNames = ["Name","Telephone","E-mail"]
+    fieldValues = []  # we start with blanks for the values
+    fieldValues = multenterbox(msg,title, fieldNames)
+# ventana borrado
+# ventana modificar
+def modify():
+    msg = "Modify the information"
+    title = "modify"
+    fieldNames = ["Name","Telephone","E-mail"]
+    fieldValues = []  # we start with blanks for the values
+    fieldValues = multenterbox(msg,title, fieldNames)
+# ventana confirmación
+def confirmation():
+    msg = "Do you want to continue?"
+    title = "Please Confirm"
+    if ccbox(msg, title):     # show a Continue/Cancel dialog
+        alert_confirm()  # user chose Continue
+    else:  # user chose Cancel
+        sys.exit(0)
+# ventana aviso de no datos
+def no_data():
+    msg = "Sorry, but I can not found any information."
+    title="Alert no data"
+    msgbox(msg,title)
+    
+# ventana de aviso de confirmación
+def alert_confirm():
+    msg = "Step is done."
+    title="Comfirm"
+    msgbox(msg,title)
+# ventana informacion programa
+def info():
+    msg = "Information of the program."
+    body = """
+            Author: Marco Baturan.
+            Program: Tool under EasyGui and
+            Peewe for store information
+            of contactcs.
+          """
+    title="Info"
+    textbox(msg,title,body)
+    
+    
+# FUNCIONES
+# crear registro
+# crear modificacion
+# crear borrado
+# crear excepcion de error
+
+
+# INICIO
+menu()
+
--- test_module.py
+++ test_module.py
@@ -3,10 +3,7 @@ import peewee
 import easygui
 import sys
 
-def test_import_agenda():
-    # Assert the existence of agenda.
-    assert agenda
-    
+
 def test_import_easygui():
     # Assert the existence of Easy GUI.
     assert easygui
@@ -18,3 +15,41 @@ def test_import_peewee():
 def test_import_sys():
     # Assert the existence of Sy.
     assert sys
+    
+def test_import_agenda():
+    # Assert the existence of agenda.
+    assert agenda
+
+def test_window_menu():
+    agenda.menu()
+    
+def test_window_confirm():
+    agenda.confirmation()
+    
+def test_window_no_data():
+    agenda.no_data()
+
+def test_window_search():
+    agenda.search()
+
+def test_window_registry():
+    agenda.registry()
+    
+def test_window_alert_confirm():
+    agenda.alert_confirm()
+
+def test_window_modify():
+    agenda.modify()
+
+# ventana informacion programa
+def test_info():
+    agenda.info()
+# FUNCIONES
+# crear registro
+# crear modificacion
+# crear borrado
+# crear excepcion de error
+
+
+# INICIO
+    
